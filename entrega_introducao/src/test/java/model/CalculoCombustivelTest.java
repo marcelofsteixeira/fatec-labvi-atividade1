71c04398e0fe62a95d2b0c25841b47368dc707db
Marcelo Teixeira <marcelofsteixeira@gmail.com>
2020-09-18 17:53:11 -0300

    Commit estado verde

--- entrega_introducao/src/test/java/model/CalculoCombustivelTest.java
+++ entrega_introducao/src/test/java/model/CalculoCombustivelTest.java
@@ -7,13 +7,13 @@ public class CalculoCombustivelTest {
 
     @Test
     public void testGastoMedio_Caso5_40(){
-        CalculoCombustivel calculoCombustivel= new CalculoCombustivel();
+        CalculoCombustivel calculoCombustivel= new CalculoCombustivelImpl();
         Assert.assertEquals(8.00f, calculoCombustivel.calcularGastoMedio(5f, 40f), 0f);
     }
 
     @Test
     public void testGastoMedio_Caso1_1(){
-        CalculoCombustivel calculoCombustivel= new CalculoCombustivel();
+        CalculoCombustivel calculoCombustivel= new CalculoCombustivelImpl();
         Assert.assertEquals(1.00f, calculoCombustivel.calcularGastoMedio(1f, 1f), 0f);
     }
 }
