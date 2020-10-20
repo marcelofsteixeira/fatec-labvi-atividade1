9f985a28519ff75757b5269c3bd01732bbcdbb60
Jessica Rosado <jessicahelen@live.com>
2020-10-19 22:36:38 -0300

    Atividade Particoes de Equivalencia - Caso Azul

--- entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/model/CalculadorViagemImpl.java
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/model/CalculadorViagemImpl.java
@@ -6,12 +6,14 @@ public class CalculadorViagemImpl implements CalculadorViagem {
 
     @Override
     public float calcularLitrosRestantesParaViagem(String carroJson, float distancia) throws Exception {
+
+        Carro carro = new Gson().fromJson(carroJson, Carro.class);
+
         if (distancia <=0) {
             throw new Exception("Distancia não pode ter valor igual a zero ou negativo!");
         }
 
-        float resultado = (distancia/new Gson().fromJson(carroJson, Carro.class).getGastoMedio()) -
-                new Gson().fromJson(carroJson, Carro.class).getQtdAbastacida();
+        float resultado = (distancia/carro.getGastoMedio()) - carro.getQtdAbastacida();
 
         if (resultado < 0) {
             return 0f;
