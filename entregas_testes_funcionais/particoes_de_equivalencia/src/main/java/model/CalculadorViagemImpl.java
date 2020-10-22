ade67dfddcc1331898207ac2d3718a9d192c0f50
Marcelo Teixeira <marcelofsteixeira@gmail.com>
2020-10-21 22:17:56 -0300

    Atividade particoes - Caso Vermelho FIX

--- entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/model/CalculadorViagemImpl.java
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/model/CalculadorViagemImpl.java
@@ -6,20 +6,7 @@ public class CalculadorViagemImpl implements CalculadorViagem {
 
     @Override
     public float calcularLitrosRestantesParaViagem(String carroJson, float distancia) throws Exception {
-
-        Carro carro = new Gson().fromJson(carroJson, Carro.class);
-
-        if (distancia <=0) {
-            throw new Exception("Distancia não pode ter valor igual a zero ou negativo!");
-        }
-
-        float resultado = (distancia/carro.getGastoMedio()) - carro.getQtdAbastacida();
-
-        if (resultado < 0) {
-            return 0f;
-        }
-
-        return resultado;
+       return null;
     }
 
 }
